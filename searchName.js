baaf9deccc314513436a9c59a06d18302080a055
Mugi Mustakim <99125333+mstkm@users.noreply.github.com>
2022-11-08 13:33:28 +0700

    Add files via upload

--- searchName.js
+++ searchName.js
@@ -1,12 +1,17 @@
 const names = ['Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn', 'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 'Olivia', 'Penelope'];
-const char = 'an';
-const limit = 3;
+// const char = 'an';
+// const limit = 3;
 
-const dataName = names.map(name => name.toLowerCase());
-function checkName (name) {
-    return name.includes(char.toLowerCase());
+function searchName (char, limit, callback) {
+    const dataName = names.map(name => name.toLowerCase());
+    const filterName = dataName.filter(name => name.includes(char.toLowerCase())).sort().slice(0, limit);
+    const resultSearch = filterName.map(name => name.charAt(0).toUpperCase() + name.slice(1));
+
+    callback(resultSearch)
 };
-const filterName = dataName.filter(checkName).sort().slice(0, limit);
-const searchNames = filterName.map(name => name.charAt(0).toUpperCase() + name.slice(1));
 
-console.log(searchNames);
+function showResult (result) {
+    console.log(result);
+}
+
+searchName('an', 3, showResult);
